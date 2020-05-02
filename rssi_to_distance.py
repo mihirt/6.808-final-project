47f83c2dffdfeab802ee53470dfb139f59c8ca54
Mihir Trivedi <mihirt@mit.edu>
2020-05-02 14:29:24 -0700

    fixefd math

--- rssi_to_distance.py
+++ rssi_to_distance.py
@@ -1,2 +1,5 @@
-def convert_rssi_to_distance(rssi, n=2.5):
-    return 10**((3000 - rssi) / (10 * n))
+def convert_rssi_to_distance(rssi, n=5):
+    return 10**((30 - (rssi / 100)) / (10 * n))
+
+
+# print(convert_rssi_to_distance(-54))
