822bb3e1e2baedda948882cebe2f7f662fe2c2f6
Bhavik Nagda <BhavikNagda@Jyotsnas-MacBook-Air-2.local>
2020-05-07 13:23:43 -0400

    Added speedup functionality

--- monodepth2/test_simple.py
+++ monodepth2/test_simple.py
@@ -163,64 +163,45 @@ def test_simple(args):
 
     print('-> Done!')
 
+"""Function to predict for a single image or folder of images
+    """
+
+model_name="mono+stereo_640x192"
+device = torch.device("cpu")
+
+download_model_if_doesnt_exist(model_name)
+model_path = os.path.join("models", model_name)
+print("-> Loading model from ", model_path)
+encoder_path = os.path.join(model_path, "encoder.pth")
+depth_decoder_path = os.path.join(model_path, "depth.pth")
+
+# LOADING PRETRAINED MODEL
+print("   Loading pretrained encoder")
+encoder = networks.ResnetEncoder(18, False)
+loaded_dict_enc = torch.load(encoder_path, map_location=device)
+
+# extract the height and width of image that this model was trained with
+feed_height = loaded_dict_enc['height']
+feed_width = loaded_dict_enc['width']
+filtered_dict_enc = {
+    k: v
+    for k, v in loaded_dict_enc.items() if k in encoder.state_dict()
+}
+encoder.load_state_dict(filtered_dict_enc)
+encoder.to(device)
+encoder.eval()
+
+print("   Loading pretrained decoder")
+depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc,
+                                        scales=range(4))
+
+loaded_dict = torch.load(depth_decoder_path, map_location=device)
+depth_decoder.load_state_dict(loaded_dict)
+
+depth_decoder.to(device)
+depth_decoder.eval()
 
 def evaluate_single(image, model_name="mono+stereo_640x192"):
-    """Function to predict for a single image or folder of images
-    """
-    # assert args.model_name is not None, \
-    #     "You must specify the --model_name parameter; see README.md for an example"
-
-    # if torch.cuda.is_available() and not args.no_cuda:
-    #     device = torch.device("cuda")
-    # else:
-    device = torch.device("cpu")
-
-    download_model_if_doesnt_exist(model_name)
-    model_path = os.path.join("models", model_name)
-    print("-> Loading model from ", model_path)
-    encoder_path = os.path.join(model_path, "encoder.pth")
-    depth_decoder_path = os.path.join(model_path, "depth.pth")
-
-    # LOADING PRETRAINED MODEL
-    print("   Loading pretrained encoder")
-    encoder = networks.ResnetEncoder(18, False)
-    loaded_dict_enc = torch.load(encoder_path, map_location=device)
-
-    # extract the height and width of image that this model was trained with
-    feed_height = loaded_dict_enc['height']
-    feed_width = loaded_dict_enc['width']
-    filtered_dict_enc = {
-        k: v
-        for k, v in loaded_dict_enc.items() if k in encoder.state_dict()
-    }
-    encoder.load_state_dict(filtered_dict_enc)
-    encoder.to(device)
-    encoder.eval()
-
-    print("   Loading pretrained decoder")
-    depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc,
-                                          scales=range(4))
-
-    loaded_dict = torch.load(depth_decoder_path, map_location=device)
-    depth_decoder.load_state_dict(loaded_dict)
-
-    depth_decoder.to(device)
-    depth_decoder.eval()
-
-    # FINDING INPUT IMAGES
-    # if os.path.isfile(args.image_path):
-    #     # Only testing on a single image
-    #     paths = [args.image_path]
-    #     output_directory = os.path.dirname(args.image_path)
-    # elif os.path.isdir(args.image_path):
-    #     # Searching folder for images
-    #     paths = glob.glob(os.path.join(args.image_path, '*.{}'.format(args.ext)))
-    #     output_directory = args.image_path
-    # else:
-    #     raise Exception("Can not find args.image_path: {}".format(args.image_path))
-    #
-    # print("-> Predicting on {:d} test images".format(len(paths)))
-
     # PREDICTING ON EACH IMAGE IN TURN
     with torch.no_grad():
         # if image_path.endswith("_disp.jpg"):
@@ -263,13 +244,6 @@ def evaluate_single(image, model_name="mono+stereo_640x192"):
                           255).astype(np.uint8)
         im = pil.fromarray(colormapped_im)
         return (scaled_disp.cpu().numpy(), im)
-        # name_dest_im = os.path.join(output_directory,
-        # "{}_disp.jpeg".format(output_name))
-        # im.save(name_dest_im)
-        #
-        # print(
-        #     "   Processed {:d} of {:d} images - saved prediction to {}".format(
-        #         idx + 1, len(paths), name_dest_im))
 
 
 if __name__ == '__main__':
